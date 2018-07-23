86eaed5d3d8d07df5f6db3c92755b0e0ce2e1c4a
Jenny Zhang <canaantt@gmail.com>
2018-07-23 15:34:12 -0700

    still missing sheet_specific_checking, but could run smoothly

--- node-process-xls/validation.js
+++ node-process-xls/validation.js
@@ -12,14 +12,12 @@ uniq_file_types.forEach(t => {
     var obj = {};
     obj.pass = false;
     obj.warning = false;
-    obj.fail = true;
     var related_files = files.filter(f => getFileType(f) === t);
     var fileobj = {};
     related_files.forEach(f => {
         fileobj[f] = {};
         fileobj[f]['pass'] = false;
         fileobj[f]['warning'] = false;
-        fileobj[f]['fail'] = true;
     });
     obj['files'] = fileobj;
     evaluation[t] = obj;
@@ -53,17 +51,17 @@ var file_types_validation_res = file_types_validation(uniq_file_types, requireme
 if (file_types_validation_res.length > 0 ){
     evaluation['warning'] = true;
     evaluation['error'] = evaluation['error'] + '| The following files cannot be incorporated into Oncoscape V3.' + file_types_validation_res;
+} else {
+    evaluation['file_types_validation'] = 'All file types can be accomodated by current Oncoscape V3 data';
 }
 
 var file_existant = function(evaluation) {
     if (!'patient' in evaluation && !'sample' in evaluation) {
         evaluation['pass'] = false;
-        evaluation['fail'] = true;
         evaluation['warning'] = false;
         evaluation['error'] = 'PATIENT or SAMPLE table does not exist.';
     } else if (!'matrix' in evaluation && !'mutation' in evaluation) {
         evaluation['pass'] = true;
-        evaluation['fail'] = false;
         evaluation['warning'] = true;
         evaluation['error'] = 'Do not have molecular data to initiate plotting.';
         evaluation['available_tools'] = ['spreadsheet'];
@@ -95,27 +93,21 @@ var file_existant = function(evaluation) {
         - genesets <-> HGNC list
 */ 
 
-var required_field = function(filename, requirement){
-    console.log(filename);
-    let data = util.loadCsv(filename);
-    const cols = util.shiftColumns(data);
-
-};
-
 files.forEach(f =>{
+    console.log(f);
     var data = util.loadCsv(f);
     var cols = util.shiftColumns(data);
-    var cols_upper = cols.map(c => c.toUpperCase());
+    var cols_upper = cols.map(k => k.toUpperCase());
     data = util.removeExtraCols(data, cols.length);
-    data = util.fillBlankNull(data);
-    data = util.removeExtraRows(data, cols.length);
+    // data = util.fillBlankNull(data);
+    // data = util.removeExtraRows(data, cols.length);
     var rows = util.shiftColumns(data);
     console.log('size of', f, ':', data.length, 'x', data[0].length);
 
     // Generic column names unique checking:
     if(!help.check_uniqueness(cols)) {
-        evaluation[type][f]['warning'] = true;
-        evaluation[type][f]['error'] = 'There is duplicated columns';
+        evaluation[type]['files'][f]['warning'] = true;
+        evaluation[type]['files'][f]['error'] = 'There is duplicated columns';
     };
 
     // Checking aginst Requirement config
@@ -124,26 +116,36 @@ files.forEach(f =>{
     if('required_fields' in r) {
         var notExistingCols = r['required_fields'].filter(k => cols_upper.indexOf(k) === -1);
         if(notExistingCols.length > 0) {
-            evaluation[type][f]['fail'] = true;
-            evaluation[type][f]['warning'] = false;
-            evaluation[type][f]['pass'] = false;
-            evaluation[type][f]['error'] = evaluation[type][f]['error'] + '|' + f + 'does not container required fields: ' + notExistingCols;
+            evaluation[type]['files'][f]['warning'] = false;
+            evaluation[type]['files'][f]['pass'] = false;
+            evaluation[type]['files'][f]['required_fields'] = notExistingCols;
+            evaluation[type]['files'][f]['error'] = evaluation[type]['files'][f]['error'] + '|' + f + 'does not container required fields';
+        } else {
+            evaluation[type]['files'][f]['warning'] = false;
+            evaluation[type]['files'][f]['pass'] = true;
+            evaluation[type]['files'][f]['required_fields'] = notExistingCols;
         }
     }
     if('unique_fields' in r) {
         var obj = {};
+        var notUniq = [];
         r['unique_fields'].forEach(k => {
             var index = cols_upper.indexOf(k);
             var col_values = data.map(d=>d[index]);
             obj[k] = help.check_uniqueness(col_values);
+            if(!obj[k]) notUniq = notUniq.concat(k);
         });
-        evaluation[type][f]['unique_fields'] = obj;
-        if(_.values(obj).indexOf(false) > -1) {
-            evaluation[type][f]['fail'] = true;
-            evaluation[type][f]['warning'] = false;
-            evaluation[type][f]['pass'] = false;
-            evaluation[type][f]['error'] = evaluation[type][f]['error'] + '|' + f + 'field(s) with replicated values:' + Object.keys(obj).filter(k => !obj(k));
-        };
+        evaluation[type]['files'][f]['unique_fields'] = obj;
+        if(notUniq.length > 0) {
+            evaluation[type]['files'][f]['warning'] = false;
+            evaluation[type]['files'][f]['pass'] = false;
+            evaluation[type]['files'][f]['unique_fields'] = notUniq;
+            evaluation[type]['files'][f]['error'] = evaluation[type]['files'][f]['error'] + '|' + f + 'field(s) with replicated values';
+        } else {
+            evaluation[type]['files'][f]['warning'] = false;
+            evaluation[type]['files'][f]['pass'] = true;
+            evaluation[type]['files'][f]['unique_fields'] = notUniq;
+        }
     }
     if('sheet_specific_checking' in r){
 
